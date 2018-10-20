93316fc9c3e3cddcdfce74ef5d145c55324b5c5f
pribytkov55@gmail.com <D8e6n-23>
2018-10-20 10:20:57 +0300

    test

--- c2/settings_prod1.py
+++ c2/settings_prod1.py
@@ -5,7 +5,7 @@ DATABASES = {
  'default': {
  'ENGINE': 'django.db.backends.postgresql_psycopg2',
  'NAME': 'db1',
- 'USER': 'shop-django',
+ 'USER': 'shop-djangopost',
  'PASSWORD': '1352Aa',
  'HOST': 'localhost',
  'PORT': '',
